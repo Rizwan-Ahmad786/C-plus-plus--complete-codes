d325cf302070eaf2f9ae3d66400ce58507fb1d48
Rizwan-Ahmad786 <65743186+Rizwan-Ahmad786@users.noreply.github.com>
2020-05-22 07:09:52 +0500

    code for knowing, that the number is even or odd.

---
+++ even  & odd number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter a number, to check that it is Even or Odd number: "<<endl;
+    cin>>num;
+    if(num%2==0)
+        cout<<num<<" is a Even number: "<<endl;
+    else
+        cout<<num<<" is a OOD number:"<<endl;
+
+return 0;
+}
+
