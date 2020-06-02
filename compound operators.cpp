87fd2c10aaec648a91096caabb38039cd9d9ac86
Rizwan-Ahmad786 <65743186+Rizwan-Ahmad786@users.noreply.github.com>
2020-06-02 16:55:05 +0500

    compound assignment operators in c++.

---
+++ compound operators.cpp
@@ -0,0 +1,22 @@
+//Compound assignment operator
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=5;
+    cout<<a<<endl;
+    a+=5;
+    cout<<a<<endl;
+    a-=5;
+    cout<<a<<endl;
+    a*=5;
+    cout<<a<<endl;
+    a/=5;
+    cout<<a<<endl;
+    a%=5;
+    cout<<a<<endl;
+    return 0;
+
+
+}
