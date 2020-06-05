9e9785eba0a515150c914e85f40ada43cc8a7e2b
Rizwan-Ahmad786 <65743186+Rizwan-Ahmad786@users.noreply.github.com>
2020-06-05 13:18:39 +0500

    program to solve this a*b/(-c*31%13)*d
    //write a program that solves the following expression.
    //a*b/(-c*31%13)*d
    //Assuming the values of variables are as follows.
    //a=10, b=20, c=15, d=8,

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
-//Increment and decrement operator
-//prefix form & profix form
+//      Program 3.9
+//write a program that solves the following expression.
+//a*b/(-c*31%13)*d
+//Assuming the values of variables are as follows.
+//a=10, b=20, c=15, d=8,
 
 #include<iostream>
 using namespace std;
 int main()
 {
-    int a,b,x,y;
-    a=b=x=y=4;
-    b=a--;
-    --x;
-    y=x;
-    cout<<"a = "<<a<<endl<<"b = "<<b<<endl;
-    cout<<"y = "<<y<<endl<<"x = "<<x<<endl;
+    int a,b,c,d,e;
+    a=10;
+    b=20;
+    c=15;
+    d=8;
+    e=a*b/(-c*31%13)*d;
+    cout<<"Result of expression is: "<<e<<endl;
     return 0;
+    
     
 }
